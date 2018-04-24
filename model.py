9ce4c201fd0d6a09b15bd445c60a76f08be8785b
Lingyun Wu <wulingyun16@gmail.com>
2018-04-24 17:41:24 -0400

    04/24/2018

--- model.py
+++ model.py
@@ -2,3 +2,80 @@ import numpy as np
 import csv
 import cv2
 import tensorflow as tf
+
+import sklearn
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Conv2D, Cropping2D
+
+
+
+
+def generator(samples, batch_size=16):
+    num_samples = len(samples)
+    correction = [0.0, 0.2, -0.2]
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                for i in range(3):
+                    name = './data/IMG/'+batch_sample[i].split('/')[-1]
+                    center_image = cv2.imread(name)
+                    center_angle = float(batch_sample[3]) + correction[i]
+                    images.append(center_image)
+                    angles.append(center_angle)
+
+            augmented_images, augmented_angles = [], []
+            for image,angle in zip(images, angles):
+                augmented_images.append(image)
+                augmented_angles.append(angle)
+                augmented_images.append(cv2.flip(image,1))
+                augmented_angles.append(angle*-1.0)
+
+            X_train = np.array(augmented_images)
+            y_train = np.array(augmented_angles)
+            yield shuffle(X_train, y_train)
+
+
+
+
+
+lines = []
+with open('./data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
+
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
+
+model = Sequential()
+model.add(Lambda(lambda x:x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
+model.add(Conv2D(24,(5,5), strides=(2,2), activation="relu"))
+model.add(Conv2D(36,(5,5), strides=(2,2), activation="relu"))
+model.add(Conv2D(48,(5,5), strides=(2,2), activation="relu"))
+model.add(Conv2D(64,(3,3), activation="relu"))
+model.add(Conv2D(64,(3,3), activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(optimizer='adam', loss='mse')
+model.fit_generator(train_generator, steps_per_epoch= len(train_samples),
+validation_data=validation_generator, validation_steps=len(validation_samples), epochs=5, verbose = 1)
+
+model.save('model.h5')
