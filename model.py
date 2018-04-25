9403568ebfa2bf23b727b0c16db57229340f7ba0
Lingyun <wulingyun16@gmail.com>
2018-04-24 23:42:32 -0400

    04/24/2018

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ from keras.layers import Flatten, Dense, Lambda, Conv2D, Cropping2D
 
 
 
-def generator(samples, batch_size=16):
+def generator(img_dir, angles, batch_size=16):
     num_samples = len(samples)
     correction = [0.0, 0.2, -0.2]
     while 1: # Loop forever so the generator never terminates
@@ -26,10 +26,10 @@ def generator(samples, batch_size=16):
             for batch_sample in batch_samples:
                 for i in range(3):
                     name = './data/IMG/'+batch_sample[i].split('/')[-1]
-                    center_image = cv2.imread(name)
-                    center_angle = float(batch_sample[3]) + correction[i]
-                    images.append(center_image)
-                    angles.append(center_angle)
+                    image = cv2.imread(name)
+                    angle = float(batch_sample[3]) + correction[i]
+                    images.append(image)
+                    angles.append(angle)
 
             augmented_images, augmented_angles = [], []
             for image,angle in zip(images, angles):
@@ -44,13 +44,33 @@ def generator(samples, batch_size=16):
 
 
 
+def loadfile(csv_dir, img_dir):
+ 
+    with open(csv_dir) as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            lines.append(line)
+
+    images_dir = []
+    angles = []
+    correction = [0.0, 0.2, -0.2]
+    for line in lines[1:]:
+        for i in range(3):
+            name = img_dir + line[i].split('/')[-1]
+            angle = float(line[3]) + correction[i]
+            images_dir.append(name)
+            angles.append(angles)
+    return images_dir, angles
 
 
-lines = []
-with open('./data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
+img_dir, angles = loadfile('./data/data1/driving_log.csv', './data/data1/IMG/')
+img_dir1, angles1 = loadfile('./data/data2/driving_log.csv', './data/data2/IMG/')
+img_dir2, angles2 = loadfile('./data/data3/driving_log.csv', './data/data3/IMG/')
+img_dir.extend(img_dir1)
+img_dir.extend(img_dir2)
+angles.extend(angles1)
+angles.extend(angles2)
+
 
 train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
